9de28e1dc631c6d75ca8cf9888109ba9f4f95c90
Jim Geist <info@jimgeist.com>
2021-03-19 19:32:05 -0400

    Blogly Part 2 completed. Refactoring next.

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 
 from flask import Flask, request, redirect, render_template, redirect, flash, session
 # from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -15,9 +16,6 @@ app.config['SECRET_KEY'] = "SECRET!"
 # debug = DebugToolbarExtension(app)
 
 connect_db(app)
-
-# once you have a design, you no longer need to call db.create_all()
-# db.create_all()
 
 
 def change_occurred(from_vals, to_vals):
@@ -48,7 +46,7 @@ def list_users():
 
     db_users = User.query.all()
 
-    return render_template("users.html", users=db_users)
+    return render_template("list_users.html", users=db_users)
 
 
 @app.route("/users/new")
@@ -74,16 +72,22 @@ def add_user_process():
     return redirect("/users")
 
 
-@app.route("/users/<user_id>")
+@app.route("/users/<int:user_id>")
 def view_user(user_id):
     """ Show the details about a single Blogly user """
 
     db_user = User.query.get_or_404(user_id)
+    # ??? do null values cause issues on the server side?
+    # I see a lot of instances in the log where an incorrect
+    # parameter is used by SQL Alchemy and they typically happen
+    # on records with nulls.
 
+    # ??? should put somthing in place to disable delete
+    # when posts exist
     return render_template("view_user.html", user=db_user)
 
 
-@app.route("/users/<user_id>/edit")
+@app.route("/users/<int:user_id>/edit")
 def edit_user(user_id):
     """ Edit the details about a single Blogly user """
 
@@ -92,7 +96,7 @@ def edit_user(user_id):
     return render_template("edit_user.html", user=db_user)
 
 
-@app.route("/users/<user_id>/edit", methods=["POST"])
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
 def edit_user_process(user_id):
     """ Process the edit of a single Blogly user """
 
@@ -113,7 +117,7 @@ def edit_user_process(user_id):
     return redirect(f"/users/{user_id}")
 
 
-@app.route("/users/<user_id>/delete", methods=["POST"])
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
 def delete_user_process(user_id):
     """ Process the delete of a single Blogly user """
 
@@ -123,3 +127,88 @@ def delete_user_process(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+
+@app.route("/posts/<int:post_id>")
+def view_post(post_id):
+    """ Show the information in a post """
+
+    db_post = Post.query.get_or_404(post_id)
+    full_name = db_post.user.get_full_name()
+    created_date = datetime.strftime(
+        db_post.created_at, "%a %b %d, %Y %I:%M %p").replace(" 0", " ")
+    return render_template("view_post.html", post=db_post, user_full_name=full_name, created=created_date)
+
+
+@app.route("/users/<int:user_id>/posts/new")
+def add_post_form(user_id):
+    """ Show form to add a post for user user_id. """
+    user_data = {}
+    user_data["id"] = user_id
+    user_data["name"] = User.query.get(user_id).get_full_name()
+
+    return render_template("add_post.html", user=user_data)
+
+
+@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+def add_post_process(user_id):
+    """ Process the add post form. The post is added and the user is
+       redirected to the user detail page, /users/<user_id> """
+
+    # extract form data, add post, commit, then redirect to /users
+    post_title = request.form["post-title"]
+    post_content = request.form["post-content"]
+
+    new_post = Post(title=post_title.strip(),
+                    content=post_content.strip(),
+                    user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/users/{ user_id }")
+
+
+@app.route("/posts/<int:post_id>/edit")
+def edit_post(post_id):
+    """ Edit the details in a single post """
+
+    post_data = {"id": post_id}
+    db_post = Post.query.get_or_404(post_id)
+    post_data["title"] = db_post.title
+    post_data["content"] = db_post.content
+    post_data["user_id"] = db_post.user_id
+
+    return render_template("edit_post.html", post=post_data)
+
+
+@app.route("/posts/<int:post_id>/edit", methods=["POST"])
+def edit_post_process(post_id):
+    """ Process the edit of a post. Return to users/<user_id> """
+
+    # db_user = User.query.get_or_404(user_id)
+    db_post = Post.query.get_or_404(post_id)
+
+    user_id = db_post.user.id
+    # extract form data, commit, then redirect to /users
+    f_title = request.form["post-title"].strip()
+    f_content = request.form["post-content"].strip()
+
+    if (change_occurred([db_post.title, db_post.content], [f_title, f_content])):
+        db_post.id = post_id
+        db_post.title = f_title
+        db_post.content = f_content
+        db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route("/posts/<int:post_id>/delete", methods=["POST"])
+def delete_post_process(post_id):
+    """ Process the delete of a single post """
+
+    db_post = Post.query.get_or_404(post_id)
+    user_id = db_post.user_id
+
+    db.session.delete(db_post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 """Models for Blogly."""
 
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, time
+import flask_sqlalchemy
+
+# flask_sqlalchemy.time.gmtime()
+# time.struct_time(tm_year=2021, tm_mon=3, tm_mday=18, tm_hour=23, tm_min=32, tm_sec=20, tm_wday=3, tm_yday=77, tm_isdst=0)
+# datetime.strptime(
+#                 cookie_data_out["date_last_activity"], "%Y-%m-%d %H:%M:%S.%f")
 
 db = SQLAlchemy()
 
@@ -15,6 +21,7 @@ def connect_db(app):
 
 # MODELS
 class User(db.Model):
+    """ User model for blobly users table  """
 
     __tablename__ = 'users'
 
@@ -42,3 +49,33 @@ class User(db.Model):
         full_name = f"{self.first_name} {self.last_name}"
 
         return full_name.strip()
+
+
+class Post(db.Model):
+    """ User model for blobly users table  """
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    title = db.Column(db.String(128),
+                      nullable=False)
+
+    content = db.Column(db.Text,
+                        nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True),
+                           default=time.strftime("%Y-%m-%d %H:%M:%S %z"),
+                           nullable=False)
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='posts')
+
+    def __repr__(self):
+        """Show post information """
+
+        return f"<Blogly #{self.id} title: '{self.title}' content: '{self.content}' created_at: {self.created_at} >"
